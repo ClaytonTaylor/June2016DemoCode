c3adc09b6748731f527d383fd244050df45fc064
Brandon Jimenez <brandon@augur.io>
2016-07-15 16:25:04 -0600

    QuickCommit

--- week3/angular-recap/js/main.js
+++ week3/angular-recap/js/main.js
@@ -8,9 +8,32 @@ angular.module('WeddingBells')
     // mapped to a function Eternity which takes no dependencies"
     .controller('BallAndChain', Eternity)
 
+// Speaking in code:
+    // 1. Set the $inject property on the Eternity controller function to a dependecy array including 'Splitsies'
+    // 2. Inject Splitsies into Eternity using $inject
+Eternity.$inject = ['divorceFactory']
+
 // Speaking in code: [create/declare/define] a function called 'Eternity'
-function Eternity() {
-    console.info('Eternity is loaded')
+function Eternity(divorceFactory) {
+    // Log levels
+    // console.error('Eternity controller is loaded')
+    // console.warn('Eternity controller is loaded')
+    console.info('Eternity.controller is loaded')
+    // console.debug('Eternity.controller.divorceFactory', divorceFactory)
+    // console.log('Eternity controller is loaded')
+    // console.debug('Eternity controller is loaded')
+
+    // `love` on the DOM is mapping to the `this` variable within the Eternity controller
+    var love = this
+
+    // Speaking in code
+    // 1. assign the property info on the love variable
+    // 2. assign an info property on the scope of the Eternity controller
+    love.info = divorceFactory.info
+
+    // we won't have to redfine average age in a future controller
+    // because the factory allows us to share METHODS as well as data
+    love.averageAgeValue = divorceFactory.averageAge()
 }
 
 // Speaking in code: "Declare a factory named 'divorceFactory'
@@ -21,6 +44,7 @@ angular.module('WeddingBells')
     .factory('divorceFactory', Splitsies)
 
 function Splitsies() {
+    console.info('Splitsies factory is loaded')
     // Speaking in code: create a collection, named data, whose SCHMEA is:
         // year with a Nuber value of 2013 and,
         // spouses with a value that is a collection with this schema...
@@ -73,6 +97,48 @@ function Splitsies() {
     // Speaking in code:
         // return an object with property/key info that has a value of the data collection
     return {
-        info: data
+        info: data,
+        averageAge: function() {
+            // Speaking in code:
+                // declare a variable named initialValue with the integer value of zero
+            var initialValue = 0
+            // STOP WRITING FOR LOOPS
+
+            // Speaking in code:
+                // declare a variable named sum which is the
+                // result of the reduction of the data array,
+                // whose callback returns an element's spouses's age property
+                // and uses the initialValue
+
+            // for(...) {
+            //     for(...){
+            //
+            //     }
+            // }
+
+            // CALLBACK HELLLLLLLL!!!
+            // don't do this!
+            // var sum = data.reduce(function(overallTotal, currentCouple){
+            //     return overallTotal + currentCouple.spouses.reduce(function(couplesTotal, currentSpouse){
+            //         return couplesTotal + currentSpouse.age
+            //     }, initialValue)
+            // }, initialValue)
+
+
+            // one way to avoid callback hell: NAME YOUR FUNCTIONS
+            var sum = data.reduce(sumCouples, initialValue);
+
+            // first-class citizens : I can use a function like a variable
+            function sumCouples (overallTotal, currentCouple) {
+                return overallTotal + currentCouple.spouses.reduce(sumSpouses, initialValue)
+            }
+
+            function sumSpouses(couplesTotal, currentSpouse) {
+                return couplesTotal + currentSpouse.age
+            }
+
+            // hey, there are two spouses! which is why we are multiplying by 2
+            return sum / (data.length*2)
+        }
     }
 }
