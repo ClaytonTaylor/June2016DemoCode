0a354ab8ec5bb094bbbe481eeda9aa7f0d50fce1
Brandon Jimenez <brandon@augur.io>
2016-07-21 09:43:30 -0600

    QuickCommit

--- week4/breakouts/error-handling/app.js
+++ week4/breakouts/error-handling/app.js
@@ -1,18 +1,34 @@
 console.info('App.js loaded!')
 
 angular.module('Chuckathon', [])
-    // .controller('ChuckingAwesomeController', ['Chuckfactory', ChuckingAwesome])
+    // .controller('ChuckingAwesomeController', ['ChuckFactory', ChuckingAwesome])
     .controller('ChuckingAwesomeController', ChuckingAwesome)
+    // .controller('ChuckingAwesomeController', ['$http', ChuckingAwesome])
     .factory('ChuckFactory', ChuckFactory)
 
 ChuckingAwesome.$inject = ['ChuckFactory']
+ChuckFactory.$inject = ['$http']
 
 function ChuckingAwesome(ChuckFactory){
     console.debug('ChuckingAwesomeController:loaded')
     console.debug('ChuckingAwesomeController:ChuckFactory', ChuckFactory)
+
+    var chuck = this
+
+    chuck.title = "Time waits for no man. Unless that man is Chuck Norris."
+    chuck.quote = "" // its really to declare default values for controller variables
+
+    ChuckFactory.getRandomQuote()
+        .then(function(){
+            
+        })
 }
 
-function ChuckFactory(){
+function ChuckFactory($http){
     console.debug('ChuckingAwesomeController:loaded')
-    return {}
+    return {
+        getRandomQuote: function() {
+            return $http.get('http://api.icndb.com/jokes/random')
+        }
+    }
 }
