deacbeb50b12d05cc298d5d28539346f2ec628bf
Brandon Jimenez <brandon@augur.io>
2016-07-21 09:29:23 -0600

    QuickCommit

--- week4/breakouts/error-handling/app.js
+++ week4/breakouts/error-handling/app.js
@@ -1,1 +1,18 @@
 console.info('App.js loaded!')
+
+angular.module('Chuckathon', [])
+    // .controller('ChuckingAwesomeController', ['Chuckfactory', ChuckingAwesome])
+    .controller('ChuckingAwesomeController', ChuckingAwesome)
+    .factory('ChuckFactory', ChuckFactory)
+
+ChuckingAwesome.$inject = ['ChuckFactory']
+
+function ChuckingAwesome(ChuckFactory){
+    console.debug('ChuckingAwesomeController:loaded')
+    console.debug('ChuckingAwesomeController:ChuckFactory', ChuckFactory)
+}
+
+function ChuckFactory(){
+    console.debug('ChuckingAwesomeController:loaded')
+    return {}
+}
