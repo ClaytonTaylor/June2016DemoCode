9fc5b5f5909b97d12d30ff97cb39b172c951483e
Brandon Jimenez <brandon@augur.io>
2016-07-21 10:14:02 -0600

    QuickCommit

--- week4/breakouts/error-handling/app.js
+++ week4/breakouts/error-handling/app.js
@@ -18,19 +18,55 @@ function ChuckingAwesome(ChuckFactory){
     chuck.title = "Time waits for no man. Unless that man is Chuck Norris."
     chuck.quote = "" // its really to declare default values for controller variables
 
-    ChuckFactory.getRandomQuote()
-        .then(function(res){
-            console.debug('getRandomQuote:res', res)
-            chuck.quote = res.data.value.joke
-            // res.data
-        })
+    chuck.clickitySplit = function () {
+        ChuckFactory.getRandomQuote()
+            // .success() // deprecated, don't use them!
+            // .failure() // deprecated, don't use them!
+            .then(function(res){
+                console.debug('getRandomQuote:res', res)
+                // chuck.quote = res.data.value.joke
+
+                // Object access safetey:
+                // this is how to safely access nested properties on a object
+                var data  = res.data   || {}
+                    value = data.value || {}
+                    joke  = value.joke || ''
+
+                chuck.quote = joke || ':( no joke found.' // UI/UX feedback
+            }, function(error) { // OMG DON"T FORGET THIS
+                // Add an error handler callback to your THEN functions!
+                // Add an error handler callback to your THEN functions!
+                // Add an error handler callback to your THEN functions!
+                // Add an error handler callback to your THEN functions!
+                // Add an error handler callback to your THEN functions!
+                // Add an error handler callback to your THEN functions!
+                // Add an error handler callback to your THEN functions!
+                // Add an error handler callback to your THEN functions!
+                // Add an error handler callback to your THEN functions!
+                // Add an error handler callback to your THEN functions!
+                // Add an error handler callback to your THEN functions!
+                console.error('ERROR:clickitySplit could not get API response:', error)
+
+                // oh nos!
+                try {
+                    // JSON.parse UNSAFE : always try catch it
+                    // unsafe code goes here
+                    chuck.norris.is.awesome
+                } catch (error) {
+                    console.log("Could not access properties", error)
+                }
+
+                chuck.quote = ':( no joke found.'
+            })
+    }
+
 }
 
 function ChuckFactory($http){
     console.debug('ChuckingAwesomeController:loaded')
     return {
         getRandomQuote: function() {
-            return $http.get('http://api.icndb.com/jokes/random')
+            return $http.get('httpiss://api.icndb.com/jokes/random')
         }
     }
 }
