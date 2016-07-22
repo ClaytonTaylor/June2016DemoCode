267417e5084e3b69b732247420410541c9892ab8
devaio <robert.camp@outlook.com>
2016-07-22 10:05:03 -0600

    Inheritance : 2 classes, WITH inheritance

--- week4/oop-inheritance/main.js
+++ week4/oop-inheritance/main.js
@@ -9,6 +9,11 @@ Wizard.prototype.castSpell = function(spellIndex){
 	return `${this.name} casts ${this.powers[spellIndex]}!`
 }
 
+Wizard.prototype.growBeard = function(){
+	this.beardLength++
+	return `${this.name} grows their beard! It now has a length of ${this.beardLength}!`
+}
+
 var smandalf = new Wizard(
 	'Smandalf the Smug', 
 	[
@@ -27,6 +32,22 @@ function Necromancer (name, powers, beardLength, minionType) {
 
 	newNecro.minionType  = minionType;
 }
+
+// First thing I need to do, is allow the Necromancer Prototype to look at the Wizard prototype
+// Necromancer.prototype = Wizard.prototype // This would create 2 variables pointing to the SAME object, the WIZARD PROTO.  I wouldn't be able to Necromancers their own methods
+
+Necromancer.prototype = new Wizard(); // This will make the Necromancer prototype a Wizard, which has access to the Wizard Prototype
+Necromancer.prototype.constructor = Necromancer; // Reset the constructor so we have the proper reference to it
+
+Necromancer.prototype.raiseMinion = function(){
+	return `${this.name} raises several ${this.minionType} from the dead...`
+}
+
+// Override the Wizard version of castSpell to make more necromancery
+// This would intercept the call of castSpell.  It would fire when it reaches the Necromancer Prototype and not continue on to the Wizard Prototype
+// Necromancer.prototype.castSpell = function(spellIndex){
+// 	return `${this.name} casts ${this.powers[spellIndex]}!  It's so super necromancery...`
+// }
 
 var wade = new Necromancer(
 	'Wade',
