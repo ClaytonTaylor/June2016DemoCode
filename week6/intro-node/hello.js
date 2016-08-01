dda113aa945bcf9bd4e4f450ed5aaa310a5ff550
devaio <robert.camp@outlook.com>
2016-08-01 15:13:37 -0600

    intro-node

--- week6/intro-node/hello.js
+++ week6/intro-node/hello.js
@@ -4,18 +4,24 @@
 // console.log('Process : ', process.memoryUsage());
 
 // FS module is used to access the file system
-var fs = require('fs');
+// var fs = require('fs');
 
-// console.log(fs);
+// // console.log(fs);
 
-fs.readFile('./hello.js', {
-	// encoding : 'utf-8'
-} ,function(err, data){
-	console.log('ERR : ', err);
-	console.log('DATA : ', data);
+// fs.readFile('./hello.js', {
+// 	// encoding : 'utf-8'
+// } ,function(err, data){
+// 	console.log('ERR : ', err);
+// 	console.log('DATA : ', data);
 
-});
+// });
 
+
+// Retrieving a moudle WE created
+var pokeymanInfo = require('./pokeymans');
+console.log(pokeymanInfo);
+
+console.log(pokeymanInfo.getPokeyman(2);
 
 
 
