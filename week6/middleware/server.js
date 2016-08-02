64f77719a03b7d47982803d69fd9a82113a5e3f4
Brandon Jimenez <brandon@augur.io>
2016-08-02 15:48:19 -0600

    QuickCommit

--- week6/middleware/server.js
+++ week6/middleware/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var logger = require('morgan');
 
 var app = express();
 
@@ -10,13 +11,46 @@ var app = express();
 // which means this will fire for
 // EVERY SINGLE REQUEST
 // vertically mounting:
-app.use(bodyParser.json()) // this attaches a POST payload to `req.body`
+// app.use(bodyParser.json()) // this attaches a POST payload to `req.body`
+// app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use( logger('dev') );
+app.use( checkKey );
+// app.use( function(req, res, next) { }) // inline-vertically-mounted middleware
 
-app.get('/', checkKey, function(req, res){ // mounting horizontally
+app.use(function(err, req, res, next){ // error-handler
+    console.error(err.stack)
+    // https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
+    res.status(500).send({
+        status: 500,
+        message: 'Hold the door!'
+    })
+});
+//
+// app.get('/', checkKey, function(req, res){ // mounting horizontally
+//     res.send('Hello squirrel!');
+// }) // often called the `root` of the site
+
+app.post('*', bodyParser.json(), bodyParser.urlencoded({ extended: true })) // lighterwight way
+
+app.get('/', function(req, res){ // mounting horizontally
     res.send('Hello squirrel!');
 }) // often called the `root` of the site
+
+// app.use( checkKey );
+
+app.get('/api*', checkKey) // horizontally mount middleware that matches a route pattern
+app.get('/api/things', function(req, res) {
+    res.send({
+        data: 'stuff'
+    })
+
+})
+app.get('/api/stuff', function(req, res) {
+    res.send({
+        data: 'stuff'
+    })
+})
 
 function checkKey(req, res, next) { // next, when called will fire the next middleware in the stack
     if( req.query.key ) {
@@ -26,7 +60,6 @@ function checkKey(req, res, next) { // next, when called will fire the next midd
             message: 'No api key'
         })
     }
-
 }
 
 // use POSTMAN to quickly test POST routes!
