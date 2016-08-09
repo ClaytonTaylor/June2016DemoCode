ee663df003d63e5fff61015bbdacc1c0fe05f7e6
devaio <robert.camp@outlook.com>
2016-08-09 11:10:07 -0600

    Front-End hook up to the backend

--- week7/heroes-of-ajax/public/scripts/app.controller.heroes.js
+++ week7/heroes-of-ajax/public/scripts/app.controller.heroes.js
@@ -5,5 +5,32 @@ heroCtrl.$inject = ['apiFactory']
 
 function heroCtrl (apiFactory){
     var hCtrl = this;
-    console.log(apiFactory)
+    hCtrl.newHero = {
+        powers     : [''], // starting with an empty array element so the ngRepeat will show HTML
+        weaknesses : ['']
+    };
+
+    hCtrl.retrieveHeroes = function(){
+        apiFactory
+            .getHeroes()
+            .then(function(response){
+                hCtrl.heroList = response.data;
+            })
+    }
+    hCtrl.retrieveHeroes();
+    // console.log(apiFactory)
+
+    hCtrl.makeAHero = function () {
+        apiFactory
+            .createHero(hCtrl.newHero)
+            .then(function(response){
+                console.log(response);
+                hCtrl.retrieveHeroes();
+            });
+    }
+
+    hCtrl.pwExtra = function (which) {
+        hCtrl.newHero[which].push('');
+    }
+
 }
