139eb257d3f9d1bd6c5b268d5d4543cd55d392eb
devaio <robert.camp@outlook.com>
2016-08-09 12:24:00 -0600

    sync

--- week7/heroes-of-ajax/models/hero.js
+++ week7/heroes-of-ajax/models/hero.js
@@ -5,14 +5,15 @@ var heroSchema = mongoose.Schema({
     name:       {type : String},
     powers:     {type : Array, default : []},
     weaknesses: {type : Array, default : []},
-    HQ:         {type : String},
-    sidekick:   {
-        name : {type : String},
-        caped: {type : Boolean}
-    },
+    // HQ:         {type : String},
+    // sidekick:   {
+    //     name : {type : String},
+    //     caped: {type : Boolean}
+    // },
     masked:     {type : Boolean, default : false},
     origin:     {type : String},
 });
+
 
 // export the model
 module.exports = mongoose.model('Hero', heroSchema, 'heroes'); // Our entrypoint into the heroes collection in the DB
