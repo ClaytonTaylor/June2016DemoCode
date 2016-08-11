1568ca903d1952f1013677ccc7e030b4d780e168
devaio <robert.camp@outlook.com>
2016-08-11 09:33:22 -0600

    backend setup for hqs

--- week7/heroes-of-ajax/routes/index.js
+++ week7/heroes-of-ajax/routes/index.js
@@ -1,4 +1,5 @@
 var heroCtrl = require('./heroes');
+var hqCtrl   = require('./hqs');
 // var otherCtrl = require('./other');
 
 module.exports = (app) => {
@@ -6,8 +7,13 @@ module.exports = (app) => {
     // Routes go here
 
     // Hero Routes
-    app.get('/api/heroes', heroCtrl.get)
-    app.post('/api/heroes', heroCtrl.upsert)
+    app.get('/api/heroes', heroCtrl.get);
+    app.post('/api/heroes', heroCtrl.upsert);
     
+    // HQ Routes
+    app.get('/api/hqs', hqCtrl.get); // Find Many
+    app.get('/api/hqs/:id', hqCtrl.get); // Find One
+    app.post('/api/hqs', hqCtrl.upsert); // Create
+    app.post('/api/hqs/:id', hqCtrl.upsert); // Update
     
 }
