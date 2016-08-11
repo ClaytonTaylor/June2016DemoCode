ec784d2a8de449bea0f65396546997ff6392eb62
devaio <robert.camp@outlook.com>
2016-08-11 09:13:57 -0600

    hq model

---
+++ week7/heroes-of-ajax/models/hq.js
@@ -0,0 +1,22 @@
+// Headquarters model
+
+var mongoose = require('mongoose');
+
+var hqSchema = mongoose.Schema({
+    name:      {type: String},
+    amenities: {type: Array},
+    butler:    {type: Boolean},
+    moatType:  {type: String},
+
+    // constructionDate : {type : Date}
+    // Sample Address
+    // address : {
+    //     street1 : String,
+    //     street2 : String,
+    //     city    : String,
+    //     state   : String,
+    //     zip     : String
+    // }
+});
+
+module.exports = mongoose.model('HQ', hqSchema);
